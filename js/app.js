83bb29669489d95c26a19e45eb7bf38901f6dee4
Ian Whitmore <ianwhitmor@gmail.com>
2021-07-27 12:11:42 -0500

    add js prompt and alert

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+console.log('app.js file is connected');
+
+// let userName = prompt('What is your name?');
+// console.log('This is the user input', userName);
+
+// alert('hello ' + userName);
+
+// let lowerCaseName = userName.toLowerCase();
+// console.log('this is bob lowercase', lowerCaseName);
+
+// if(lowerCaseName === 'ian' || lowerCaseName === 'bob'){
+//     alert('Welcome back to my page,' + ' ' + userName + '.');
+// } else {
+//     alert('Welcome new user, to my page')
+// }
+
