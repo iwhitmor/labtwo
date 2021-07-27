40832eb96756eaab097756afa89239336bb636cd
Ian Whitmore <ianwhitmor@gmail.com>
2021-07-27 17:22:20 -0500

    update js content

--- js/app.js
+++ js/app.js
@@ -18,14 +18,13 @@ console.log('app.js file is connected');
 
 // Prompt the user a total of exactly five yes or no questions. The user input for the answer must accept either y/n or yes/no responses while taking into consideration case sensitivity by normalizing the user input so that it can be validated (hint: look into the .toUpperCase() or .toLowerCase() functions). Be sure to let the user know if they answered the question correctly by alerting them with a response.
 
-// Add console.log() messages within your app to notify the user if they are correct. Before submitting, comment out (don’t delete) your console.log() messages and replace them with the alerts() to complete the project.
 
 // Display the user’s name back to them in your final message to the user.
 
-// let userName = prompt('Please enter your full name', "<name goes here>");
-// console.log('This is the user full name', userName);
+let userName = prompt('Please enter your full name', "<name goes here>");
+console.log('This is the user full name', userName);
 
-// alert('Welcome to my site, ' + userName + '. ' + 'I hope you learn a lot about me today.');
+alert('Welcome to my site, ' + userName + '. ' + 'I hope you learn a lot about me today.');
 
 let favoriteSport = prompt('Is my favorite sport golf?');
 if (favoriteSport === 'yes' || favoriteSport === 'y') {
