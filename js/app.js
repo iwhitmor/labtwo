75c3a2e2dee5cba07a33638fcfed8a06d22d9b72
Ian Whitmore <ianwhitmor@gmail.com>
2021-07-27 22:31:32 -0500

    add alert to content

--- js/app.js
+++ js/app.js
@@ -1,26 +1,6 @@
 'use strict';
 console.log('app.js file is connected');
 
-// let userName = prompt('What is your name?');
-// console.log('This is the user input', userName);
-
-// alert('hello ' + userName);
-
-// let lowerCaseName = userName.toLowerCase();
-// console.log('this is bob lowercase', lowerCaseName);
-
-// if(lowerCaseName === 'ian' || lowerCaseName === 'bob'){
-//     alert('Welcome back to my page,' + ' ' + userName + '.');
-// } else {
-//     alert('Welcome new user, to my page')
-// }
-
-
-// Prompt the user a total of exactly five yes or no questions. The user input for the answer must accept either y/n or yes/no responses while taking into consideration case sensitivity by normalizing the user input so that it can be validated (hint: look into the .toUpperCase() or .toLowerCase() functions). Be sure to let the user know if they answered the question correctly by alerting them with a response.
-
-
-// Display the user’s name back to them in your final message to the user.
-
 let userName = prompt('Please enter your full name', "<name goes here>");
 console.log('This is the user full name', userName);
 
@@ -60,3 +40,5 @@ if (dominantHand === 'no' || dominantHand === 'n') {
 } else {
   alert('BOOOO, wrong!');
 }
+
+alert('That was the last question, ' + userName + '. ' + 'You sorta did a great job. Have a nice day!')
