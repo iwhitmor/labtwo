a98c1dacf70a14cd64e794dd8fd409be71459e80
Ian Whitmore <ianwhitmor@gmail.com>
2021-07-29 16:40:22 -0500

    take comments off js page

--- js/app2.js
+++ js/app2.js
@@ -1,103 +1,103 @@
 'use strict';
 console.log('app2.js file is connected');
 
-// alert('Welcome to my quiz page. Answer all of the questions correctly to win a prize (that is fake and not real). Good luck, have fun!');
+alert('Welcome to my quiz page. Answer all of the questions correctly to win a prize (that is fake and not real). Good luck, have fun!');
 
-// function questionOne() {
-//   let favoriteSport = prompt('Is my favorite sport golf?');
-//   let lowerCaseSport = favoriteSport.toLowerCase();
-//   if (lowerCaseSport === 'yes' || lowerCaseSport === 'y') {
-//     alert('DING DING DING, correct!');
-//   } else {
-//     alert('BOOOO, wrong!');
-//   }
-//   console.log('Answer to favorite sport', lowerCaseSport);
-// }
-// questionOne();
+function questionOne() {
+  let favoriteSport = prompt('Is my favorite sport golf?');
+  let lowerCaseSport = favoriteSport.toLowerCase();
+  if (lowerCaseSport === 'yes' || lowerCaseSport === 'y') {
+    alert('DING DING DING, correct!');
+  } else {
+    alert('BOOOO, wrong!');
+  }
+  console.log('Answer to favorite sport', lowerCaseSport);
+}
+questionOne();
 
-// function questiontwo() {
-//   let favoriteFood = prompt('Is my favorite food tuna fish in a can?');
-//   let lowerCaseFood = favoriteFood.toLowerCase();
-//   if (lowerCaseFood === 'no' || lowerCaseFood === 'n') {
-//     alert('DING DING DING, correct!');
-//   } else {
-//     alert('BOOOO, wrong!');
-//   }
-//   console.log('Answer to favorite food', lowerCaseFood);
-// }
-// questiontwo();
+function questiontwo() {
+  let favoriteFood = prompt('Is my favorite food tuna fish in a can?');
+  let lowerCaseFood = favoriteFood.toLowerCase();
+  if (lowerCaseFood === 'no' || lowerCaseFood === 'n') {
+    alert('DING DING DING, correct!');
+  } else {
+    alert('BOOOO, wrong!');
+  }
+  console.log('Answer to favorite food', lowerCaseFood);
+}
+questiontwo();
 
-// function questionThree() {
-//   let stateBorn = prompt('Was I born in New York State?');
-//   let lowerCaseBorn = stateBorn.toLowerCase();
-//   if (lowerCaseBorn === 'yes' || lowerCaseBorn === 'y') {
-//     alert('DING DING DING, correct!');
-//   } else {
-//     alert('BOOOO, wrong!');
-//   }
-//   console.log('Answer to state born', lowerCaseBorn);
-// }
-// questionThree();
+function questionThree() {
+  let stateBorn = prompt('Was I born in New York State?');
+  let lowerCaseBorn = stateBorn.toLowerCase();
+  if (lowerCaseBorn === 'yes' || lowerCaseBorn === 'y') {
+    alert('DING DING DING, correct!');
+  } else {
+    alert('BOOOO, wrong!');
+  }
+  console.log('Answer to state born', lowerCaseBorn);
+}
+questionThree();
 
-// function questionFour() {
-//   let favoriteMusic = prompt('Is my favorite type of music Reggae?');
-//   let lowerCaseMusic = favoriteMusic.toLowerCase();
-//   if (lowerCaseMusic === 'yes' || lowerCaseMusic === 'y') {
-//     alert('DING DING DING, correct!');
-//   } else {
-//     alert('BOOOO, wrong!');
-//   }
-//   console.log('Answer to favorite music', lowerCaseMusic);
-// }
-// questionFour();
+function questionFour() {
+  let favoriteMusic = prompt('Is my favorite type of music Reggae?');
+  let lowerCaseMusic = favoriteMusic.toLowerCase();
+  if (lowerCaseMusic === 'yes' || lowerCaseMusic === 'y') {
+    alert('DING DING DING, correct!');
+  } else {
+    alert('BOOOO, wrong!');
+  }
+  console.log('Answer to favorite music', lowerCaseMusic);
+}
+questionFour();
 
-// function questionFive() {
-//   let dominantHand = prompt('Am and right handed?');
-//   let lowerCaseHand = dominantHand.toLowerCase();
-//   if (lowerCaseHand === 'no' || lowerCaseHand === 'n') {
-//     alert('DING DING DING, correct!');
-//   } else {
-//     alert('BOOOO, wrong!');
-//   }
-//   console.log('Answer to dominant hand', lowerCaseHand);
-// }
-// questionFive();
+function questionFive() {
+  let dominantHand = prompt('Am and right handed?');
+  let lowerCaseHand = dominantHand.toLowerCase();
+  if (lowerCaseHand === 'no' || lowerCaseHand === 'n') {
+    alert('DING DING DING, correct!');
+  } else {
+    alert('BOOOO, wrong!');
+  }
+  console.log('Answer to dominant hand', lowerCaseHand);
+}
+questionFive();
 
-// alert('Now lets play a number guessing game!');
+alert('Now lets play a number guessing game!');
 
-// function questionSix() {
-//   let attempts = 6;
-//   let actualNumber = 11;
+function questionSix() {
+  let attempts = 6;
+  let actualNumber = 11;
 
-//   while (attempts > 0) {
+  while (attempts > 0) {
 
-//     let userNumber = prompt('Can you guess my favorite number (between 1 and 50)? This is the number I wore on all my jerseys while playing sports during my illustrious career. You only have 6 guesses to guess correctly.', '<number goes here>');
-//     console.log(typeof (userNumber));
-//     userNumber = parseInt(userNumber);
-//     console.log('the string after parseInt ', userNumber);
-//     if (!userNumber) {
-//       break;
-//     }
+    let userNumber = prompt('Can you guess my favorite number (between 1 and 50)? This is the number I wore on all my jerseys while playing sports during my illustrious career. You only have 6 guesses to guess correctly.', '<number goes here>');
+    console.log(typeof (userNumber));
+    userNumber = parseInt(userNumber);
+    console.log('the string after parseInt ', userNumber);
+    if (!userNumber) {
+      break;
+    }
 
-//     if (userNumber > actualNumber) {
-//       alert('That guess was too high. Try again!');
-//     } else if (userNumber < actualNumber) {
-//       alert('That guess was too low. Try again!');
-//     } else {
-//       alert('You got it right. Nice job!');
-//       console.log('did this work? they guessed right.');
-//       break;
-//     }
+    if (userNumber > actualNumber) {
+      alert('That guess was too high. Try again!');
+    } else if (userNumber < actualNumber) {
+      alert('That guess was too low. Try again!');
+    } else {
+      alert('You got it right. Nice job!');
+      console.log('did this work? they guessed right.');
+      break;
+    }
 
-//     attempts = attempts - 1;
-//   }
+    attempts = attempts - 1;
+  }
 
-//   alert('Your all done. The correct answer was 11.');
-// }
+  alert('Your all done. The correct answer was 11.');
+}
 
-// questionSix();
+questionSix();
 
-// alert('Just a few more questions about food. Here we go!');
+alert('Just a few more questions about food. Here we go!');
 
 let favoriteFood = ['wings', 'pizza', 'burgers', 'meatloaf', 'sushi', 'BBQ'];
 console.log(favoriteFood.length);
